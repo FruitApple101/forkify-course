b57eedbc428ef839b0d850443d0c9d2d7b20168c
FruitApple101 <juanpino12345678910@gmail.com>
2024-08-27 13:33:55 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'fdb35c61-37db-4e7d-899b-b2d38f3f6834';
+export const MODAL_CLOSE_SEC = 2.5;
